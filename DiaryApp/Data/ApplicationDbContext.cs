596e764f8a2cb905f0671cd0e0aa88fe9d3b8431
Nerijus <Nerijus@DESKTOP-08FUU9C>
2024-09-10 22:53:58 +0300

    Create new oject and seeded data to data base

--- DiaryApp/Data/ApplicationDbContext.cs
+++ DiaryApp/Data/ApplicationDbContext.cs
@@ -1,15 +1,47 @@
 ﻿using DiaryApp.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
 
 namespace DiaryApp.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
 
         public DbSet<DiaryEntry> DiaryEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DiaryEntry>().HasData(
+                new DiaryEntry 
+                { 
+                    Id = 1, 
+                    Title = "Went Hiking", 
+                    Content = "Went hiking with Joe!", 
+                    Created = DateTime.Now
+                },
+                new DiaryEntry
+                {
+                    Id = 2,
+                    Title = "Went Shopping",
+                    Content = "Went Shopping with Joe!",
+                    Created = DateTime.Now
+                },
+                new DiaryEntry
+                {
+                    Id = 3,
+                    Title = "Went Swiming",
+                    Content = "Went Swiming with Joe!",
+                    Created = DateTime.Now
+                }
+                ); 
+        }
     }
 }
---
+++ DiaryApp/Migrations/20240910195121_AddedSeedingDataDiaryEntry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DiaryApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSeedingDataDiaryEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "DiaryEntries",
+                columns: new[] { "Id", "Content", "Created", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Went hiking with Joe!", new DateTime(2024, 9, 10, 22, 51, 20, 470, DateTimeKind.Local).AddTicks(6681), "Went Hiking" },
+                    { 2, "Went Shopping with Joe!", new DateTime(2024, 9, 10, 22, 51, 20, 470, DateTimeKind.Local).AddTicks(6689), "Went Shopping" },
+                    { 3, "Went Swiming with Joe!", new DateTime(2024, 9, 10, 22, 51, 20, 470, DateTimeKind.Local).AddTicks(6695), "Went Swiming" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
