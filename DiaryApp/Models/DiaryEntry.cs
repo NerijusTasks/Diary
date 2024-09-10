24a3e33660aa03241e2d658cb12c4c2fbf6335b0
Nerijus <Nerijus@DESKTOP-08FUU9C>
2024-09-10 20:48:22 +0300

    Add project files.

---
+++ DiaryApp/Models/DiaryEntry.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp.Models
+{
+    public class DiaryEntry
+    {
+        //[Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Title { get; set; }
+        [Required]
+        public string? Content { get; set; }
+        [Required]
+        public DateTime Created { get; set; } = DateTime.Now; 
+    }
+}
